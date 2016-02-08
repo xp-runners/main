63436fba73c531cc7aabe826d20ad6ac4555f8b7
Timm Friebe <timm.friebe@1und1.de>
2016-02-08 18:27:56 +0100

    Make `xp script` work without the necessity for a ".php" file extension

--- src/class-main.php
+++ src/class-main.php
@@ -77,29 +77,23 @@ foreach ($argv as $i => $val) {
   $argv[$i]= iconv('utf-7', \xp::ENCODING, $val);
 }
 
-$ext= substr($argv[0], -4, 4);
-if ('.php' === $ext) {
-  if (false === ($uri= realpath($argv[0]))) {
-    throw new \Exception('Cannot load '.$argv[0].' - does not exist');
-  }
-  if (null === ($cl= \lang\ClassLoader::getDefault()->findUri($uri))) {
-    if (strstr($uri, '.class.php')) {
+if (is_file($argv[0])) {
+  if (0 === substr_compare($argv[0], '.class.php', -10)) {
+    $uri= realpath($argv[0]);
+    if (null === ($cl= \lang\ClassLoader::getDefault()->findUri($uri))) {
       throw new \Exception('Cannot load '.$uri.' - not in class path');
     }
+    $class= $cl->loadUri($uri);
+  } else if (0 === substr_compare($argv[0], '.xar', -4)) {
+    $cl= \lang\ClassLoader::registerPath(realpath($argv[0]));
+    if (!$cl->providesResource('META-INF/manifest.ini')) {
+      throw new \Exception($cl->toString().' does not provide a manifest');
+    }
+    $class= $cl->loadClass(parse_ini_string($cl->getResource('META-INF/manifest.ini'))['main-class']);
+  } else {
     $class= \lang\XPClass::forName('xp.runtime.Evaluate');
     array_unshift($argv, 'eval');
-  } else {
-    $class= $cl->loadUri($uri);
   }
-} else if ('.xar' === $ext) {
-  if (false === ($uri= realpath($argv[0]))) {
-    throw new \Exception('Cannot load '.$argv[0].' - does not exist');
-  }
-  $cl= \lang\ClassLoader::registerPath($uri);
-  if (!$cl->providesResource('META-INF/manifest.ini')) {
-    throw new \Exception($cl->toString().' does not provide a manifest');
-  }
-  $class= $cl->loadClass(parse_ini_string($cl->getResource('META-INF/manifest.ini'))['main-class']);
 } else {
   $class= \lang\ClassLoader::getDefault()->loadClass($argv[0]);
 }
