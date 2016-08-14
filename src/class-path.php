cba5cf4f7cd9a411c076c4199cb32c0f65e39a46
Timm Friebe <thekid@thekid.de>
2016-08-14 18:44:06 +0200

    Revert last commit

--- src/class-path.php
+++ src/class-path.php
@@ -1,6 +1,5 @@
 <?php namespace xp;
 
-$paths= array_merge($bootstrap['overlay'], $bootstrap['core'], $bootstrap['local']);
 foreach ($bootstrap['files'] as $file) {
   require $file;
 }
@@ -9,6 +8,7 @@ if (class_exists('xp', false)) {
   foreach ($bootstrap['overlay'] as $path) { \lang\ClassLoader::registerPath($path, true); }
   foreach ($bootstrap['local'] as $path) { \lang\ClassLoader::registerPath($path); }
 } else if (isset($bootstrap['base'])) {
+  $paths= array_merge($bootstrap['overlay'], $bootstrap['core'], $bootstrap['local']);
   require $bootstrap['base'];
 } else {
   $parts= explode(PATH_SEPARATOR.PATH_SEPARATOR, get_include_path());
