d8953b76b2f8a2dc9ae184dde815ebcb67e9a293
Timm Friebe <thekid@thekid.de>
2021-12-07 11:43:15 +0100

    Revert xp-runners/main@df9cfda719eadbc10759b232c21b9e833aa4067b
    We can instead pass autoload files to `-cp` without any changes

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -27,11 +27,7 @@ function bootstrap($cwd, $home) {
     } else if ('~' === $path[0]) {
       $path= $home.substr($path, 1);
     }
-    if (is_file($path)) {
-      $result['files'][]= $path;
-    } else {
-      scanpath($result, pathfiles($path), $path, $home);
-    }
+    scanpath($result, pathfiles($path), $path, $home);
   }
 
   // Always add current directory
